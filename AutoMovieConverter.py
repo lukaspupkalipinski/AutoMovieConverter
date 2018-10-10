06ab6df6b11419d4ac7318a47c4c0f411047f89d
lukaspupkalipinski <lukas.pupka.lipinski@owlet.de>
2018-10-10 02:07:36 +0200

    solved some bugs. added the case if tool is runing twice on the directory.

--- AutoMovieConverter.py
+++ AutoMovieConverter.py
@@ -13,7 +13,7 @@ __author__ = "Lukas Pupka-Lipinski"
 __copyright__ = "Copyright 2018, AutoMovieConverter"
 __credits__ = [""]
 __license__ = "GPL"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __maintainer__ = "Lukas Pupka-Lipinski"
 __email__ = "support@lpl-mind.de"
 __status__ = "Dev"
@@ -21,7 +21,9 @@ __status__ = "Dev"
 
 FFmpegx264command="ffmpeg -i \"%s\" -c:v libx264 -preset slow -crf 22 -c:a copy \"%s\"-converted.mkv"
 FFMpegcheckcommand="ffmpeg -i \"%s\" 2>&1 | grep \"Duration\""
+FFMpegcheck2command="ffmpeg -i \"%s\" 2>&1 | grep \"ENCODER\""
 Removecommand="rm -f \"%s\""
+Movecommand="mv \"%s\" \"%s\""
 
 filter=r".*\.(avi|mkv|flv|flv|avi|MTS|M2TS|mov|wmv|mp4|m4v|mpg|mp2|mpeg|mpe|mpv|mpg|mpeg|m2v|m4v|flv|f4v)"
 
@@ -44,16 +46,46 @@ def checkvalid(origin, converted):
 
     searchtime = r"Duration:\s([0-9]{2}):([0-9]{2}):([0-9]{2}).([0-9]{2}),\sstart"
 
+    if (len(orignoutput)==0):
+        return False
+    if (len(convertedoutput)==0):
+        return False
+
+    orignoutput=orignoutput[0]
+    convertedoutput=convertedoutput[0]
+
+    if (orignoutput==None):
+        return False
+    if (convertedoutput==None):
+        return False
+
     re1 = re.findall(searchtime, orignoutput)
     re2 = re.findall(searchtime, convertedoutput)
+
+    if (not re1 or not re2):
+        return False
+
+    if (len(re1)==0 and len(re2)==0):
+        return False
+
+    if (len(re1[0])!=4 and len(re2[0])!=4):
+        return False
 
     if (re1[0][0]==re2[0][0] and re1[0][1]==re2[0][1]):
         return True
     else:
         return False
 
-checkvalid("","")
+def needconvert(file):
+    process = subprocess.Popen(FFMpegcheck2command % file,
+                               shell=True, stdout=subprocess.PIPE)
+    process.wait()
+    output = process.communicate()
 
+    if (len(output)>=1):
+        if ("libx264" in output[0]):
+            return False
+    return True
 parser = argparse.ArgumentParser(description='Convert all Movie files to a specific format.')
 parser.add_argument('infile', metavar='inputfile', nargs=1,
                    help='input folder')
@@ -80,21 +112,39 @@ f = []
 for (dirpath, dirnames, filenames) in walk(path):
     for filename in filenames:
         match=re.match(filter,filename)
-        if match:
-            #is media file
+        if match and needconvert(dirpath+os.sep+filename):
             justname = filename.split(".")[:-1][0]
+            orign=dirpath+os.sep+filename
+            convered=dirpath+os.sep+justname+"-converted.mkv"
 
-            if (not os.path.isfile(dirpath+os.sep+justname+"-converted.mkv")):
-                process = subprocess.Popen(FFmpegx264command%(dirpath+os.sep+filename,dirpath+os.sep+justname), shell=True, stdout=subprocess.PIPE)
+
+            #remove if convered file exists but is corrupted
+            if (os.path.isfile(convered)):
+                if (not checkvalid(orign,convered)):
+                    process = subprocess.Popen(
+                        Removecommand % (convered), shell=True,
+                        stdout=subprocess.PIPE)
+                    process.wait()
+
+
+            if (not os.path.isfile(convered)):
+                process = subprocess.Popen(FFmpegx264command%(orign,dirpath+os.sep+justname), shell=True, stdout=subprocess.PIPE)
                 process.wait()
 
-            if (remove and checkvalid(dirpath+os.sep+filename,dirpath+os.sep+justname+"-converted.mkv")):
+            #remove file and rename if file is valid
+            if (remove and checkvalid(orign,convered)):
                 process = subprocess.Popen(
-                    Removecommand % (dirpath+os.sep+justname+"-converted.mkv"), shell=True,
+                    Removecommand % (orign), shell=True,
                     stdout=subprocess.PIPE)
                 process.wait()
 
-            pass
+                process = subprocess.Popen(
+                    Movecommand % (convered,orign), shell=True,
+                    stdout=subprocess.PIPE)
+                process.wait()
+
+
+
 
     if(not recursive):
         break
